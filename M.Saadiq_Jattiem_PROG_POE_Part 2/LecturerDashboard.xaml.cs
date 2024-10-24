c4fb49aeee06d76d9ed96151951c5cb567fa4668
Saadiq Jattiem <ST10303758@rcconnect.edu.za>
2024-10-24 12:54:05 +0000

    Fixed ;all the data errors

--- M.Saadiq_Jattiem_PROG_POE_Part 2/LecturerDashboard.xaml.cs
+++ M.Saadiq_Jattiem_PROG_POE_Part 2/LecturerDashboard.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Data.SqlClient;
 
 namespace M.Saadiq_Jattiem_PROG_POE_Part_2
 {
--- M.Saadiq_Jattiem_PROG_POE_Part 2/SubmitClaim.xaml.cs
+++ M.Saadiq_Jattiem_PROG_POE_Part 2/SubmitClaim.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace M.Saadiq_Jattiem_PROG_POE_Part_2
 {
